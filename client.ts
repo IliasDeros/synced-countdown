f87d404ec5d7cc4bf47ea87e4ca33114144b87a6
Ilias Deros <IliasDeros@users.noreply.github.com>
2021-08-29 19:29:10 -0400

    Initial commit

---
+++ client.ts
@@ -0,0 +1,31 @@
+import Pusher from "pusher-js";
+
+const appKey = process.env.PUSHER_APP_KEY;
+
+const startCountdown = () => {
+  alert("start countdown");
+};
+
+const subscribeTimer = () => {
+  // Enable pusher logging - don't include this in production
+  Pusher.logToConsole = true;
+
+  const pusher = new Pusher(appKey!, {
+    cluster: "mt1"
+  });
+
+  const channel = pusher.subscribe("countdown");
+  channel.bind("start", startCountdown);
+};
+
+const showAppKeyError = () => {
+  document.querySelector("#js-app")!.innerHTML = `
+    <span class="text-error">Fatal Error: Missing PUSHER_APP_KEY</span>
+  `;
+};
+
+if (appKey) {
+  subscribeTimer();
+} else {
+  showAppKeyError();
+}
