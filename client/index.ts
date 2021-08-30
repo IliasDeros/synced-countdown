541f86fc1bc30b760d4fa1d90595b6e78e38a6f0
Ilias Deros <IliasDeros@users.noreply.github.com>
2021-08-29 20:38:55 -0400

    Add countdown UI

--- client/index.ts
+++ client/index.ts
@@ -1,30 +1,42 @@
 import Pusher from "pusher-js";
+import { animateCountdown } from "./clock";
 
 const appKey = process.env.KEY;
 const cluster = process.env.CLUSTER;
 
-const startCountdown = () => {
-  alert("start countdown");
+const showError = (message: string) => {
+  document.querySelector("#js-app")!.innerHTML = `
+    <span class="text-error">Uh oh! ${message}</span>
+  `;
+};
+
+const showAppKeyError = () => {
+  showError("Missing environment variable KEY or CLUSTER");
+};
+
+const showApiError = () => {
+  showError("Failed to send message to api");
+};
+
+const startCountdown = async () => {
+  const res = await fetch("/api/start-countdown");
+  if (!res.ok) {
+    showApiError();
+  }
 };
 
 const subscribeTimer = () => {
+  if (!appKey || !cluster) {
+    return showAppKeyError();
+  }
   // Enable pusher logging - don't include this in production
   Pusher.logToConsole = true;
 
   const pusher = new Pusher(appKey!, { cluster });
 
   const channel = pusher.subscribe("countdown");
-  channel.bind("start", startCountdown);
+  channel.bind("start", animateCountdown);
 };
 
-const showAppKeyError = () => {
-  document.querySelector("#js-app")!.innerHTML = `
-    <span class="text-error">Fatal Error: Missing environment variable KEY or CLUSTER</span>
-  `;
-};
-
-if (appKey && cluster) {
-  subscribeTimer();
-} else {
-  showAppKeyError();
-}
+document.querySelector("#js-start")!.addEventListener("click", startCountdown);
+subscribeTimer();
